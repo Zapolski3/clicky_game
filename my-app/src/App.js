d56a488c1f06d65fa7b769fa728eb6ca88b41f75
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2019-06-22 14:23:52 -0400

    added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 import Header from './components/Header'
 import Body from './components/body'
 import Footer from './components/footer'
+import Image from './components/Image'
+
 
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <img src="../public/body_image.png"/>
      <Body />
      <Footer />
     </div>
