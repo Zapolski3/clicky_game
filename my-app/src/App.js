27b98974f95d87930c09edae4048bd1ec3d85688
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2019-06-22 01:15:27 -0400

    added components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import Body from './components/body'
+import Footer from './components/footer'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js this is the home work bla bla bla</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <Body />
+     <Footer />
     </div>
   );
 }
