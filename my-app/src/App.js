46d8e896d5c2ea3e0b02984adf1ab6be3cc62bac
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2019-06-22 14:59:38 -0400

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
 import Body from './components/body'
-import Footer from './components/footer'
-import Image from './components/Image'
+// import Footer from './components/footer'
+
 
 
 
@@ -12,9 +12,9 @@ function App() {
   return (
     <div className="App">
      <Header />
-     <img src="../public/body_image.png"/>
+     
      <Body />
-     <Footer />
+     {/* <Footer /> */}
     </div>
   );
 }
